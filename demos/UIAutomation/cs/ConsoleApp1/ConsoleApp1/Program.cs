457e7350df6d40033fd65677d6f69341274343a9
root <2407439@qq.com>
2018-05-23 16:45:58 +0800

    UIAutomation解析UI组件太慢了，吐槽一下！！！

--- demos/UIAutomation/cs/ConsoleApp1/ConsoleApp1/Program.cs
+++ demos/UIAutomation/cs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,17 +9,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(DateTime.Now + " -> 正在解析UI组件");
             var window = AutomationElement.FromHandle(new IntPtr(397890));
             var area = window.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
+            StringBuilder build = new StringBuilder();
             if (area != null)
             {
-                StringBuilder build = new StringBuilder();
                 build.AppendLine("Name:" + area.Current.Name);
                 build.AppendLine("CalssName:" + area.Current.ClassName);
                 build.AppendLine("Handle:" + area.Current.NativeWindowHandle);
                 build.AppendLine("Rect:" + area.Current.BoundingRectangle);
                 build.AppendLine("Value:" + ((ValuePattern)area.GetCurrentPattern(ValuePattern.Pattern)).Current.Value);
             }
+            Console.WriteLine(DateTime.Now + " -> 解析结果: \r\n" + build.ToString());
+            Console.ReadKey();
         }
     }
 }
